13e4133cdeb870cb64975dfb7966ca583fd7af85
AhmedChabayta <chabays@gmail.com>
2022-11-23 10:58:38 +0200

    push

--- components/Layout.js
+++ components/Layout.js
@@ -11,22 +11,24 @@ function Layout() {
         <Nav />
         <Banner />
       </header>
-      <Row title="TRENDING" fetchUrl={requests.fetchTrending} />
-      <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
-      <Row title="ACTION" fetchUrl={requests.fetchActionMovies} />
-      <Row title="COMEDY" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="HORROR" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="ROMANCE" fetchUrl={requests.fetchRomanceMovies} />
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetlixOriginals}
-        isLargeRow
-      />
-      <Row title="MYSTERY" fetchUrl={requests.fetchMystery} />
-      <Row title="SCIFI" fetchUrl={requests.fetchSciFi} />
-      <Row title="WESTERN" fetchUrl={requests.fetchWestern} />
-      <Row title="ANIMATION" fetchUrl={requests.fetchAnimation} />
-      <Row title="TV" fetchUrl={requests.fetchTV} />
+      <main className="flex flex-col h-[100%] w-[100%] m-0 space-y-0">
+        <Row title="TRENDING" fetchUrl={requests.fetchTrending} />
+        <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
+        <Row title="ACTION" fetchUrl={requests.fetchActionMovies} />
+        <Row title="COMEDY" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="HORROR" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="ROMANCE" fetchUrl={requests.fetchRomanceMovies} />
+        <Row
+          title="NETFLIX ORIGINALS"
+          fetchUrl={requests.fetchNetlixOriginals}
+          isLargeRow
+        />
+        <Row title="MYSTERY" fetchUrl={requests.fetchMystery} />
+        <Row title="SCIFI" fetchUrl={requests.fetchSciFi} />
+        <Row title="WESTERN" fetchUrl={requests.fetchWestern} />
+        <Row title="ANIMATION" fetchUrl={requests.fetchAnimation} />
+        <Row title="TV" fetchUrl={requests.fetchTV} />
+      </main>
     </div>
   );
 }
